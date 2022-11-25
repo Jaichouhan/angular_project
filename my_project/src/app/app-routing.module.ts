41bf0ad6114b6c5a38bef6ec09117d5057d7baf1
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-11-25 14:46:53 +0530

    angular routes add

--- my_project/src/app/app-routing.module.ts
+++ my_project/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApiDataComponent } from './api-data/api-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:ApiDataComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
